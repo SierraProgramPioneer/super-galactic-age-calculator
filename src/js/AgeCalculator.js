024a5003b5741cb4dab5489ebfbf42a318384aee
SierraProgramPioneer <melissaparker520@gmail.com>
2024-01-30 15:29:44 -0800

    Passes Jest Test for creating a human constructor

--- src/js/AgeCalculator.js
+++ src/js/AgeCalculator.js
@@ -1,3 +1,7 @@
 export default class AgeCalculator {
-
+    constructor(pastAge, currentAge, futureAge) {
+        this.pastAge = pastAge;
+        this.currentAge = currentAge;
+        this.futureAge = futureAge;
+    }
 }

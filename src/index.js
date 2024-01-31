646bdc69a1811329f55d2460e52d924339306274
SierraProgramPioneer <melissaparker520@gmail.com>
2024-01-30 18:40:47 -0800

    Finalize changes for 1/30/2024

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
 import "./css/styles.css";
-alert("hello");
+
+
+
+// Get Age
+
+// Update User Interface
+
+// Handle Form Submission
+function handleFormSubmission(event) {
+    event.preventDefault();
+    console.log("event");
+}
+
+
+window.addEventListener("load", function () {
+    const submitButton = document.getElementById("submit");
+    submitButton.addEventListener("submit", handleFormSubmission);
+});

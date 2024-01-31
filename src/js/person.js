549c01ea1dc980bf16c4919237dfea1060d720e7
SierraProgramPioneer <melissaparker520@gmail.com>
2024-01-30 16:08:22 -0800

    Pass jest test to create new person

--- src/js/person.js
+++ src/js/person.js
@@ -1,4 +1,8 @@
 export default class SpaceAgeCalculator {
 
-
+    constructor(pastAge, currentAge, futureAge) {
+        this.pastAge = pastAge;
+        this.currentAge = currentAge;
+        this.futureAge = futureAge;
+    }
 }
